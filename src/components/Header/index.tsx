4bc6595e9fd0db02ac6c34675c9c46df3a9db241
ihmdevcont <ihmdevcont@gmail.com>
2023-10-13 14:31:13 -0700

    Updating react-auth to 0.0.5

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from 'react';
+import { MouseEvent } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "@pangeacyber/react-auth";
 
@@ -7,24 +7,25 @@ const Header = () => {
 
   const handleLogout = (e: MouseEvent) => {
     e.preventDefault();
-    logout(false);
-  }
+    logout();
+  };
 
   return (
     <div className="header">
-      <img src="/pangea-logo.svg" alt="Pangea Logo"/>
-      { authenticated && (
+      <img src="/pangea-logo.svg" alt="Pangea Logo" />
+      {authenticated && (
         <div className="nav">
           <Link to="/">Home</Link>
           <Link to="/profile">Profile</Link>
         </div>
       )}
-      { authenticated ? 
-        <button onClick={handleLogout}>Logout</button> :
+      {authenticated ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
         <button onClick={login}>Login</button>
-      }
+      )}
     </div>
   );
-}
+};
 
 export default Header;
