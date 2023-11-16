99fbbd92e3950bd04c792ff21b5d3b7fcb3c19e3
Shweta <shwetakale144@gmail.com>
2023-11-16 12:15:05 +0530

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="footer">Made By Shweta For <a href="http://localhost:3000/">Hashnode</a> x <a href="https://pangea.cloud/">Pangea Securethon</a></div>
+    <div className="footer">Made By &nbsp;<a href="https://twitter.com/shweta_kale1">Shweta</a>&nbsp; For &nbsp;<a href="http://localhost:3000/">Hashnode</a> &nbsp;x&nbsp; <a href="https://pangea.cloud/">Pangea</a>&nbsp; Securethon&nbsp;</div>
   );
 }
 
